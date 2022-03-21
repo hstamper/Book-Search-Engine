23a15f75b77031446cb2e9ba331b0cd740548e82
Hilliard Stamper <hilliard.stamper@att.net>
2022-03-21 10:26:51 -0400

    updated server files

--- server/server.js
+++ server/server.js
@@ -1,43 +1,42 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
-const { ApolloServer } = require('apollo-server-express');
+const express = require("express");
+// import ApolloServer
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
+const path = require("path");
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const startServer = async () => {
-  // create a new Apollo server and pass in our schema data
-  const server = new ApolloServer({ 
-    typeDefs, 
-    resolvers, 
-    context: authMiddleware 
-  });
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
-  // Start the Apollo server
-  await server.start();
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
 
-  // integrate our Apollo server with the Express application as middleware
-  server.applyMiddleware({ app });
-
-  // log where we can go to test our GQL API
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-};
-
-// Initialize the Apollo server
-startServer();
-
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  });
 }
 
-app.use(routes);
+// app.use(routes);
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  // log where we can go to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
